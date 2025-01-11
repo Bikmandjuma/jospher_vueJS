ec66810911fae17b7cb351122c20ab5d43b7e27e
Bikman Ntiruhungwa <bikmangeek@gmail.com>
2025-01-11 02:50:03 +0200

    package modified

--- src/api.js
+++ src/api.js
@@ -1,6 +1,6 @@
 // import axios from 'axios';
 
-// const flaskApiUrl = 'http://192.168.0.82:8000/api';
+// const flaskApiUrl = 'http://127.0.0.1:5000/api';
 
 const flaskApiUrl ='https://jospherflask-production.up.railway.app/api';
 const laravelApiUrl = 'http://127.0.0.1:8000/api';
