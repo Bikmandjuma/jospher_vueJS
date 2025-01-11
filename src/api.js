6a84f9e81ba27308f89eab76161bb81d76f977a3
Bikman Ntiruhungwa <bikmangeek@gmail.com>
2025-01-11 02:31:27 +0200

    package modified

--- src/api.js
+++ src/api.js
@@ -2,7 +2,7 @@
 
 // const flaskApiUrl = 'http://192.168.0.82:8000/api';
 
-const flaskApiUrl ='https://jospherflask-production.up.railway.app/api/';
+const flaskApiUrl ='https://jospherflask-production.up.railway.app/api';
 const laravelApiUrl = 'http://127.0.0.1:8000/api';
 
 export { flaskApiUrl, laravelApiUrl };
