9fd9f81387485100dd89934b0a56da1919d0fadb
Bikman Ntiruhungwa <bikmangeek@gmail.com>
2025-02-17 23:15:29 +0200

    payment content modified

--- src/api.js
+++ src/api.js
@@ -4,6 +4,6 @@
 // const laravelApiUrl = 'http://127.0.0.1:8000/api';
 
 const flaskApiUrl ='https://jospherflask-production.up.railway.app/api';
-const laravelApiUrl ="https://jospherlaravelapi-production.up.railway.app/api";
+const laravelApiUrl ="https://jospher.up.railway.app/api";
 
 export { flaskApiUrl, laravelApiUrl };
