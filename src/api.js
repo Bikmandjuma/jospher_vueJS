4175291043ee55de5c0e09c53f8a5090fe9a14e9
Bikman Ntiruhungwa <bikmangeek@gmail.com>
2025-05-14 22:58:46 +0200

    api route added

--- src/api.js
+++ src/api.js
@@ -4,7 +4,7 @@
 // const laravelApiUrl = 'http://127.0.0.1:8000/api';
 // const laravelApiUrl ="http://192.168.0.82:8000/api"
 
-const flaskApiUrl ='https://jospherflask.up.railway.app/api';
-const laravelApiUrl ="https://jospher.up.railway.app/api";
+const flaskApiUrl ='https://jospherflaskapi.up.railway.app/api';
+const laravelApiUrl ="https://jospherlaravelapi.up.railway.app/api";
  
 export { flaskApiUrl, laravelApiUrl };
