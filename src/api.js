6a5e40f42e4baf823fdae5e9394bdea082821496
Mariam0401 <umutesimariam04@gmail.com>
2025-07-05 11:46:37 +0200

    laravel and flask urls modified

--- src/api.js
+++ src/api.js
@@ -4,7 +4,7 @@
 // const laravelApiUrl = 'http://127.0.0.1:8000/api';
 // const laravelApiUrl ="http://192.168.0.82:8000/api"
 
-const flaskApiUrl ='https://jospherflaskapi.up.railway.app/api';
-const laravelApiUrl ="https://jospherlaravelapi.up.railway.app/api";
+const flaskApiUrl ='https://jospherflaskapi-production.up.railway.app/api';
+const laravelApiUrl ="jospherlaravelapi-production.up.railway.app/api";
  
 export { flaskApiUrl, laravelApiUrl };
